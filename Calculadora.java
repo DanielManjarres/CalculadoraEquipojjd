18700cf6cf60b6e23bb1a463f94b2e7cae2c7c0e
Daniel <danielmanjarres6@gmail.com>
2025-04-28 17:13:11 -0500

    Agregada función sumar

--- Calculadora.java
+++ Calculadora.java
@@ -1,5 +1,10 @@
 public class Calculadora {
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
         System.out.println("¡Calculadora del Equipo JJD!");
+        System.out.println("Suma: " + sumar(5, 3)); // Prueba suma
     }
 }
